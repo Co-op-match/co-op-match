822c861312e1b7ab520d11a2f0a8392592fdf7b3
Pachh23 <wamagant29@gmail.com>
2025-09-01 23:05:59 +0700

    add

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -32,8 +32,6 @@ import AdminPostDetailManagement from "./pages/Admin/post/PostDetail";
 import LikedPosts from "./pages/LikedPost/LikedPosts";
 import CompanyProfileView from "./pages/Profile/Company/CompanyProfileView";
 import AdminVerify from "./pages/Admin/verify/verify";
-import ChatInterface from "./Chat/ChatInterface";
-import ChatWebSocket from "./Chat/test";
 import AdvancedChatInterface from "./logo.png/ChatInterface";
 
 import AdminUser from "./pages/Admin/user/main";
